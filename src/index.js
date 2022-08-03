f89ce3b7fa7f1b82c481633a7a39286fd46f16b8
abdullah_mamun <aamun22@gmail.com>
2022-08-03 06:45:54 -0700

    card design

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 // index.css import
 // import './index.css'
 
-const title = "Welcome";
-const subtitle = "To my-app";
+
+const title = "To Do App";
+const todotilte = "My Card"
 const desc = "Wo es du sanftes ja du gar ort lange. Im friedlich komm wie sanftes mal, hast der du ergötzt helle." ;
 const date = new Date();
 const dateName = date.getDate();
 const monthName = date.getMonth();
 const yearName = date.getFullYear();
+
 // Internal CSS
 // const headingStyle={
 //   backgroundColor:"purple",
@@ -21,9 +23,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
     <h1 className='headingStyle largeText'>{title}</h1>
-    <h2>{subtitle}</h2>
-    <p>{desc}</p>
-    <p>{dateName+"/"+monthName+"/"+yearName}</p>
+    
+    <div className='card'>
+      <h3 className='cardTitle'>{todotilte}</h3>
+      <p className='cardDesc'>{desc}</p>
+      <p className='cardFooter'>{dateName+"/"+monthName+"/"+yearName}</p>
+    </div>
+    
   </div>
 
 
