5dc33ba957752383dc2c8764ce73d933c2d9f2da
abdullah_mamun <aamun22@gmail.com>
2022-08-10 06:15:27 -0700

    Nested Mapping

--- src/App.js
+++ src/App.js
@@ -16,13 +16,35 @@ function App(){
    */
   // items =Data.map((item)=> <Card textTitle ={item.Name}desig={item.Designation} descText={item.desc}/>)
 
+  const users =[
+    {
+      FullName:"Abdullah Al Mamun",
+      Age:32,
+      phones : [{Home:"0178956221",Office:"01789458231"}]
+    },
+    {
+      FullName:"Abdullah Al Hasan",
+      Age:32,
+      phones : [{Home:"0176221",Office:"01789451"}]
+    }
+  ]
+
 
     return  <div>
     {/* <h1 className='headingStyle largeText'>To Do App</h1>
     {/* {items} */}
     {/* {Data.map((item,index)=> <Card key={index} textTitle ={item.Name}desig={item.Designation} descText={item.desc}/>)} */} 
       
-      <List />
+      {/* <List /> */}
+      <h1>Nasted Lists</h1>
+      {users.map((user,index)=> (<article key={index}>
+        <h3>Full Name:{user.FullName}</h3>
+        <p>Age:{user.Age}</p>
+        {user.phones.map((phone,index)=>(<div key={index}>
+          <p>Home:{phone.Home}</p>
+          <p>Office:{phone.Office}</p>
+        </div>))}
+      </article>))}
       </div>
 
 }
